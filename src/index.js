363f101c2d839c0e94e7b72219bd6ab129aaa429
Nateyana Blake <nateyanablake@MacBook-Pro-6.local>
2019-10-16 12:26:24 -0400

    Getting comfortable with jsx synax

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import ReactDOM from 'react-dom';
 // Create a react component - A react component is a function or a class.
 // Purpose: To produce HTML to display to the user (using JSX) & handle feedback from the user (Using Event Handlers)
 const App = () => {
-    return <div>Hi There!</div>;
+
+    const buttonText = 'Click Me!';
+
+    return (
+        <div>
+            {/* must use className instead of class -> may conflict with javaScript 'class' keyword */}
+            <label className="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            {/* Inline styles -> must be placed inside of curly braces and written as js objects + jsx provides the ability to reference javascript variables  */}
+            <button style={{backgroundColor: 'pink' , color: 'white'}}>{buttonText}</button>
+        </div>
+    );
 };
 
 //Take the react component and show it on the screen
